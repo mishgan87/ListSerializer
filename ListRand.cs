dc5763b6af27f5eeb9cdf3dbca386ea2fdd0c5e7
Mikhail <Mikhail@DESKTOP-H81DTFE>
2024-03-12 10:58:14 +0300

    Добавьте файлы проекта.

---
+++ ListRand.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ListSerializer
+{
+    public class ListRand
+    {
+        public ListNode Head;
+        public ListNode Tail;
+        public int Count;
+        public void Serialize(Stream s)
+        {
+            // Словарь "ListNode - Индекс"
+            var nodes = new Dictionary<ListNode, int>();
+            for (var (node, index) = (Head, 0); node != null; node = node.Next, index++)
+            {
+                nodes.Add(node, index);
+            }
+
+            using (var writer = new BinaryWriter(s, Encoding.UTF8, false))
+            {
+                for (var node = Head; node != null; node = node.Next)
+                {
+                    // Пишем ListNode.Data и индекс ListNode.Rand (-1 если ListNode.Rand равен null)
+                    writer.Write(node.Data);
+                    writer.Write(node.Rand != null ? nodes[node.Rand] : -1);
+                }
+            }
+        }
+
+        public void Deserialize(Stream s)
+        {
+            // Словарь "Индекс ListNode - (данные, индекс ListNode.Rand )"
+            var nodes = new Dictionary<int, (string data, int randIndex)>();
+            var count = 0;
+            using (var reader = new BinaryReader(s, Encoding.UTF8, false))
+            {
+                while (reader.PeekChar() != -1)
+                {
+                    var data = reader.ReadString();
+                    var randIndex = reader.ReadInt32();
+                    nodes.Add(count++, (data, randIndex));
+                }
+
+                Count = count;
+                var nodesList = new List<ListNode>(Count);
+
+                Head = new ListNode();
+                for (var (node, i) = (Head, 0); node != null && i < Count; node = node.Next, i++)
+                {
+                    nodesList.Add(node);
+                    node.Data = nodes[i].data;
+                    // Проверка на Tail ListNode для связи Prev с Next
+                    if (i < Count - 1)
+                    {
+                        node.Next = new ListNode { Prev = node };
+                    }
+                    else
+                    {
+                        Tail = node;
+                    }
+                }
+
+                for (var (node, i) = (Head, 0); node != null && i < Count; node = node.Next, i++)
+                {
+                    // Если индекс -1, пишем в ListNode.Rand null
+                    node.Rand = nodes[i].randIndex != -1 ? nodesList[nodes[i].randIndex] : null;
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListSerializer
+{
+    class Program
+    {
+        /// <summary>
+        /// Генерирует ListRand
+        /// </summary>
+        /// <param name="count">Количество ListNode</param>
+        /// <returns>ListRand</returns>
+        public static ListRand GetListRand(int count)
+        {
+            var random = new Random();
+            var nodes = new List<ListNode>();
+            for (var i = 0; i < count; i++)
+            {
+                nodes.Add(new ListNode { Prev = null, Next = null, Rand = null, Data = $"{random.Next(0, count)}" });
+            }
+            var rands = count;
+            for (var i = 0; i < nodes.Count; i++)
+            {
+                var node = nodes[i];
+
+                if (i > 0)
+                    node.Prev = nodes[i - 1];
+                if (i != nodes.Count - 1)
+                    node.Next = nodes[i + 1];
+
+                if (rands > 0)
+                {
+                    var randomNodeIndex = random.Next(0, count);
+                    node.Rand = nodes[randomNodeIndex];
+                    rands--;
+                }
+            }
+            return new ListRand { Head = nodes.First(), Tail = nodes.Last(), Count = nodes.Count };
+        }
+        /// <summary>
+        /// Вывод в консоль данных, хранящихся в ListRand
+        /// </summary>
+        /// <param name="head">Первый ListNode</param>
+        public static void PrintListRand(ListNode head)
+        {
+            Console.WriteLine($"_______________________________________________{Environment.NewLine}");
+            var node = head;
+            while (node != null)
+            {
+                Console.WriteLine($"Node: {node.Data} \tPrev: {node.Prev?.Data ?? "NoData"} \tNext: {node.Next?.Data ?? "NoData"} \tRand: {node?.Data ?? "NoData"}");
+                node = node.Next;
+            }
+            Console.WriteLine($"_______________________________________________{Environment.NewLine}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество ListNode : ");
+            var count = Convert.ToInt32(Console.ReadLine());
+            var listRand = GetListRand(count);
+            
+            // Путь к файлу для хранения сериализованных данных
+            var fileName = @"data.dat";
+
+            listRand.Serialize(new FileStream(fileName, FileMode.Create));
+            PrintListRand(listRand.Head);
+
+            try
+            {
+                listRand.Deserialize(new FileStream(fileName, FileMode.Open));
+                PrintListRand(listRand.Head);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Не удалось обработать файл {fileName} : {ex.Message}");
+            }
+            
+            Console.WriteLine($"{Environment.NewLine}Нажмите любую клавишу...{Environment.NewLine}");
+            Console.ReadKey();
+        }
+    }
+}
